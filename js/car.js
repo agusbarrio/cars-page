733eee97ad859b580c5bb231780626c698ef2bdf
Agustin Barrio <agusbarrioeastman@gmail.com>
2023-03-06 20:53:40 -0300

    termino detalle de auto

--- js/car.js
+++ js/car.js
@@ -1,53 +1,117 @@
 import data from '../carsFake.json' assert { type: 'json' };
 
+const d = document;
+
+//get data
 const carsList = data.cars;
 const params = new URLSearchParams(window.location.search);
 const vinNumberParam = params.get('vinNumber');
 const car = carsList.find((el) => el.vinNumber === vinNumberParam);
-const d = document;
 
-/* 
-Vehicle Information
-Year: 2006
-Make: Honda
-Model: Civic
-VIN Number: 1HGFA16806L000001
-Pictures: https://example.com/honda-civic-1.jpg, https://example.com/honda-civic-2.jpg
-Status: Active
-Owner: John Doe
-License Plate: ABC123
-Registration Date: 2020-01-01
-Registration Image: https://example.com/honda-civic-registration.jpg
+//templates
+const $cardTemplate = d.getElementById('card-template');
+const $cardLineTemplate = d.getElementById('card-line-template');
+const $figureTemplate = d.getElementById('figure-template');
 
-Insurance Information
-Insurance Date: 2020-02-01
-Insurance Card: https://example.com/honda-civic-insurance.jpg
-Insurance Company: ABC Insurance
-Agent Name: John Smith
-Phone: 555-555-5555
-Policy Number: ABC12345
+//containers
+const $container = d.getElementById('container');
+const $cardsContainer = d.getElementById('cards-container');
+const $figuresContainer = d.getElementById('figures-container');
 
-Additional Information
-Oil Change Date: 2020-03-01
-Notes:
-Millage: 25000
-Turo: No
-Turo Status:
-SunPass Number:
-*/
+//load data
+const getCardLine = (label, value) => {
+  const $cardLine = $cardLineTemplate.content.cloneNode(true);
+  $cardLine.getElementById('card-line-label').textContent = label;
+  $cardLine.getElementById('card-line-value').textContent = value;
+  return $cardLine;
+};
+
+const loadVehicleInformation = ({
+  year,
+  make,
+  model,
+  vinNumber,
+  status,
+  owner,
+  licencePlate,
+  registrationDate,
+  registrationImage,
+}) => {
+  const $card = $cardTemplate.content.cloneNode(true);
+  $card.getElementById('card-title').textContent = 'Vehicle information';
+  const $cardBody = $card.getElementById('card-body');
+  $cardBody.appendChild(getCardLine('Year', year));
+  $cardBody.appendChild(getCardLine('Make', make));
+  $cardBody.appendChild(getCardLine('Model', model));
+  $cardBody.appendChild(getCardLine('VIN number', vinNumber));
+  $cardBody.appendChild(getCardLine('Status', status));
+  $cardBody.appendChild(getCardLine('Owner', owner));
+  $cardBody.appendChild(getCardLine('Licence plate', licencePlate));
+  $cardBody.appendChild(getCardLine('Registration date', registrationDate));
+  $cardsContainer.appendChild($card);
+};
+
+const loadInsuranceInformation = ({
+  insuranceDate,
+  insuranceCompany,
+  agentName,
+  phone,
+  policyNumber,
+  insuranceCard,
+}) => {
+  const $card = $cardTemplate.content.cloneNode(true);
+  $card.getElementById('card-title').textContent = 'Insurance information';
+  const $cardBody = $card.getElementById('card-body');
+  $cardBody.appendChild(getCardLine('Insurance date', insuranceDate));
+  $cardBody.appendChild(getCardLine('Insurance company', insuranceCompany));
+  $cardBody.appendChild(getCardLine('Agent name', agentName));
+  $cardBody.appendChild(getCardLine('Phone', phone));
+  $cardBody.appendChild(getCardLine('Policy number', policyNumber));
+  $cardsContainer.appendChild($card);
+};
+
+const loadAdditionalInformation = ({
+  oilChangeDate,
+  notes,
+  millage,
+  turo,
+  turoStatus,
+  sunPassNumber,
+}) => {
+  const $card = $cardTemplate.content.cloneNode(true);
+  $card.getElementById('card-title').textContent = 'Additional information';
+  const $cardBody = $card.getElementById('card-body');
+  $cardBody.appendChild(getCardLine('Oil change date', oilChangeDate));
+  $cardBody.appendChild(getCardLine('Notes', notes));
+  $cardBody.appendChild(getCardLine('Millage', millage));
+  $cardBody.appendChild(getCardLine('Turo', turo));
+  $cardBody.appendChild(getCardLine('Turo status', turoStatus));
+  $cardBody.appendChild(getCardLine('sunPassNumber', sunPassNumber));
+
+  $cardsContainer.appendChild($card);
+};
+
+const loadFigure = (src, label) => {
+  const $figure = $figureTemplate.content.cloneNode(true);
+  const $img = $figure.getElementById('figure-img');
+  $img.src = src;
+  $img.onerror = () => {
+    $img.src = './assets/image.svg';
+  };
+  $figure.getElementById('figure-figcaption').textContent = label;
+  $figuresContainer.appendChild($figure);
+};
 
 const $title = d.getElementById('car-title');
 if (car) {
   $title.textContent = `${car.year} ${car.make} ${car.model}`;
-  const $template = d.getElementById('car-info-template');
+  const $carouselTemplate = d.getElementById('carousel-template');
   const $carouselItemTemplate = d.getElementById('carousel-item-template');
-  const $clone = $template.content.cloneNode(true);
+  const $clone = $carouselTemplate.content.cloneNode(true);
   const $mainImg = $clone.getElementById('main-img');
   const $carouselItemsContainer = $clone.getElementById(
     'carousel-items-container'
   );
-
-  const $container = d.getElementById('container');
 
   $mainImg.src = car.mainPicture;
   const pictures = car.pictures.split(', ');
@@ -60,9 +124,13 @@ if (car) {
     };
     $carouselItemsContainer.appendChild($carouselItemClone);
   });
+
   $container.appendChild($clone);
-  //TODO seguir agregando info
-  //TODO ver que no se deformen las imagenes
+  loadVehicleInformation(car);
+  loadInsuranceInformation(car);
+  loadAdditionalInformation(car);
+  loadFigure(car.registrationImage, 'Registration');
+  loadFigure(car.insuranceCard, 'Insurance');
 } else {
   $title.textContent = 'Car not found';
 }
